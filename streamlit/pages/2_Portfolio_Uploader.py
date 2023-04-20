5f81ba0b71a9b10517a1b7329a3d16e3574c39f9
JaredVidelefsky <jared.videlefsky@gmail.com>
2023-04-20 00:06:50 -0400

    Added option to manually upload stocks in UI

--- streamlit/pages/2_Portfolio_Uploader.py
+++ streamlit/pages/2_Portfolio_Uploader.py
@@ -27,49 +27,85 @@ st.title("Stock Analysis Summarizer")
 # Uploading User Stock Portfolio
 file = st.file_uploader("**Upload your Stock Portfolio**", type=["xlsx"])
 
-# If the user has uploaded a file
-if file is not None:
-    # Read the Excel file into a Pandas DataFrame
-    df = pd.read_excel(file, sheet_name='Sheet1')
-    # Display the contents of the DataFrame
-    st.write(df)
+# Initialize
+if "portfolio" not in st.session_state:
+    st.session_state['portfolio'] = pd.DataFrame(columns=['Stock_Ticker'])
 
-# Selecting Ticker for Analysis from the List Available
-add_item = st.text_input("**Add Ticker**")
-if add_item:
-    # Load the Excel file
-    workbook = openpyxl.load_workbook("C:/Users/rumij/Downloads/ticker.xlsx")
-    # Select the worksheet you want to add the item to
-    worksheet = workbook["Sheet1"]
-    # Add a new item to the worksheet
-    new_item = add_item
-    worksheet.append([new_item])
-    # Save the changes to the Excel file
-    workbook.save("C:/Users/rumij/Downloads/ticker.xlsx")
+# Manually add tickers
+manual_upload = st.checkbox('Manually Upload Portfolio')
 
-remove_item = st.text_input("**Delete a Ticker**")
-if remove_item:
-    # Load the Excel file
-    workbook = openpyxl.load_workbook("C:/Users/rumij/Downloads/ticker.xlsx")
-    # Select the worksheet you want to add the item to
-    worksheet = workbook["Sheet1"]
-    # Add a new item to the worksheet
-    for row in worksheet.iter_rows():
-        for cell in row:
-            if cell.value == remove_item:
-            #     # Delete the row containing the cell
-                worksheet.delete_rows(cell.row)
-                # Save the changes to the Excel file
-                workbook.save("C:/Users/rumij/Downloads/ticker.xlsx")
-                break  # Exit the inner loop after deleting the row
+def add_data(ticker):
+    st.session_state['portfolio'] = st.session_state['portfolio'].append({'Stock_Ticker': ticker}, ignore_index=True)
 
-my_list = []
-stocks = pd.read_excel('C:/Users/rumij/Downloads/ticker.xlsx')
-stocks = stocks['Ticker'].unique()
-# selected_option = st.selectbox("**Select an option**", stocks)
-selected_option = st.multiselect("**Select Any 5 Tickers for Analysis**", stocks, max_selections=2)
+if manual_upload:
+    portfolio = pd.DataFrame(columns=['Stock_Ticker'])
 
-# my_list.append(selected_option)
-my_list = 'You selected: ' + ', '.join(selected_option)
+    # Only can run 5 stocks at a time
+    if st.session_state['portfolio'].shape[0] <= 4:
 
-st.write(my_list)
+        ticker = st.text_input("Enter a ticker:")
+
+        if ticker:
+            add_data(ticker)
+            st.session_state['portfolio'] = st.session_state['portfolio']
+    
+    else:
+        st.error('You can only run 5 stocks at a time!')
+
+    st.write(st.session_state['portfolio'])
+
+
+# Run Analysis
+run_analysis = st.checkbox('Run Analysis')
+
+if run_analysis:
+    # TODO:
+    st.write('INSERT INTO SNOWFLAKE AND TRIGGER DAG')
+
+
+# # If the user has uploaded a file
+# if file is not None:
+#     # Read the Excel file into a Pandas DataFrame
+#     df = pd.read_excel(file, sheet_name='Sheet1')
+#     # Display the contents of the DataFrame
+#     st.write(df)
+
+# # Selecting Ticker for Analysis from the List Available
+# add_item = st.text_input("**Add Ticker**")
+# if add_item:
+#     # Load the Excel file
+#     workbook = openpyxl.load_workbook("C:/Users/rumij/Downloads/ticker.xlsx")
+#     # Select the worksheet you want to add the item to
+#     worksheet = workbook["Sheet1"]
+#     # Add a new item to the worksheet
+#     new_item = add_item
+#     worksheet.append([new_item])
+#     # Save the changes to the Excel file
+#     workbook.save("C:/Users/rumij/Downloads/ticker.xlsx")
+
+# remove_item = st.text_input("**Delete a Ticker**")
+# if remove_item:
+#     # Load the Excel file
+#     workbook = openpyxl.load_workbook("C:/Users/rumij/Downloads/ticker.xlsx")
+#     # Select the worksheet you want to add the item to
+#     worksheet = workbook["Sheet1"]
+#     # Add a new item to the worksheet
+#     for row in worksheet.iter_rows():
+#         for cell in row:
+#             if cell.value == remove_item:
+#             #     # Delete the row containing the cell
+#                 worksheet.delete_rows(cell.row)
+#                 # Save the changes to the Excel file
+#                 workbook.save("C:/Users/rumij/Downloads/ticker.xlsx")
+#                 break  # Exit the inner loop after deleting the row
+
+# my_list = []
+# stocks = pd.read_excel('C:/Users/rumij/Downloads/ticker.xlsx')
+# stocks = stocks['Ticker'].unique()
+# # selected_option = st.selectbox("**Select an option**", stocks)
+# selected_option = st.multiselect("**Select Any 5 Tickers for Analysis**", stocks, max_selections=2)
+
+# # my_list.append(selected_option)
+# my_list = 'You selected: ' + ', '.join(selected_option)
+
+# st.write(my_list)
