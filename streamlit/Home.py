cfcd82e84278ddb900b8c5f0064a0b2909e6370a
manideepakreddyaila <aila.m@northeastern.edu>
2023-04-20 22:44:26 -0400

    changes

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,12 +1,10 @@
 import streamlit as st
-from components import login, register, portfolio_uploader, dummy
+from components import login, register, portfolio_uploader, admin_dashboard, analysis_vs_reality
+import requests
 
 # Check if the user is logged in
 if 'access_token' not in st.session_state:
     st.session_state.access_token = ''
-    page = 'Login'
-else:
-    page = 'Dummy'
 
 # Define a function to render the sidebar
 def render_sidebar():
@@ -14,26 +12,34 @@ def render_sidebar():
     if st.session_state.access_token == '':
         selected_page = st.sidebar.selectbox("Select a page", ["Login", "Register"])
     else:
-        selected_page = st.sidebar.selectbox("Select a page", ["Portfolio Uploader", "Dummy"])
-
+        selected_page = st.sidebar.selectbox("Select a page", ["Portfolio Uploader", "Analysis vs Reality", "Admin Dashboard"])
+        response = requests.get("http://localhost:8000/user_info", params={'token': st.session_state.access_token})
+        email = response.json().get('email')
+        if email:
+            st.sidebar.write(f"Logged in as: {email}")
+        if st.sidebar.button('Logout'):
+            st.session_state.access_token = ''
+            st.experimental_rerun()
     return selected_page
 
 # Render the sidebar
 selected_page = render_sidebar()
 
 # Render the selected page
-if selected_page == 'Welcome':
-    st.title("Welcome to my app!")
-    st.write("Please log in or register to use the app.")
-
-elif selected_page == 'Login':
+if selected_page == 'Login':
     login.login()
 
 elif selected_page == 'Register':
     register.register()
 
 elif selected_page == 'Portfolio Uploader':
-    portfolio_uploader.portfolio_uploader()
+    # portfolio_uploader.portfolio_uploader()
+    pass
 
-elif selected_page == 'Dummy':
-    dummy.dummy()
+elif selected_page == 'Analysis vs Reality':
+    # portfolio_uploader.portfolio_uploader()
+    pass
+
+elif selected_page == 'Admin Dashboard':
+    # portfolio_uploader.portfolio_uploader()
+    pass
