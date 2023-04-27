e8120079349990c01fe0dd19da0f44dd1b37bdf4
JaredVidelefsky <jared.videlefsky@gmail.com>
2023-04-27 15:32:24 -0400

    Finalized analysis vs reality page

--- streamlit/components/analysis_vs_reality.py
+++ streamlit/components/analysis_vs_reality.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 import plotly.express as px
 import pandas as pd
+import datetime
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 from Util import db_util
@@ -21,7 +22,7 @@ def analysis_vs_reality():
     selected_stock = st.selectbox('Select a stock', stock_list)
     if selected_stock:
         date_list = []
-        date_list.append(df.loc[df['STOCK'] == selected_stock, 'RUN_DATE'].item())
+        date_list.extend(df.loc[df['STOCK'] == selected_stock, 'RUN_DATE'].tolist())
         date_list = list(set(date_list))
 
         selected_date = st.selectbox('Select a date of analysis', date_list)
@@ -29,8 +30,6 @@ def analysis_vs_reality():
     # Get data from logging table
     analysis_df = db_util.analysis_results(st.session_state.email, selected_stock, selected_date)
     st.write(analysis_df)
-
-    # TODO: Add performance metrics since the run_date
 
     # TODO: Use the run dates in the graphs below
 
@@ -60,4 +59,40 @@ def analysis_vs_reality():
         fig = make_subplots(rows=1, cols=1)
         fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Close', line=dict(color='royalblue', width=2)))
         fig.update_layout(title=f'{selected_stock} Stock Price', xaxis_title='Date', yaxis_title='Price ($)', font=dict(family='Courier New, monospace', size=18, color='#7f7f7f'))
+
+        # Highlight section of graph since run
+        # Define the x-range for the highlighted section
+        x_start = selected_date
+        x_end = pd.to_datetime('today')
+
+        # Create a rectangular shape to highlight the section
+        fig.update_layout(
+            shapes=[
+                go.layout.Shape(
+                    type="rect",
+                    xref="x",
+                    yref="paper",
+                    x0=x_start,
+                    y0=0,
+                    x1=x_end,
+                    y1=1,
+                    fillcolor="blue",
+                    opacity=0.2,
+                    layer="below",
+                    line_width=0,
+                )
+            ]
+        )
+
         st.plotly_chart(fig)
+
+        # Change in stock 
+        analysis_price = data.loc[selected_date.strftime('%Y-%m-%d'), 'Close']
+        end_price = data['Close'][-1]
+        st.write(f'Price during Analysis: {analysis_price: .2f} vs. Current Price {end_price: .2f}')
+        change = (end_price - analysis_price) / analysis_price
+
+        if change > 0:
+            st.subheader(f'{selected_stock} has changed :green[{change: .2f}%] since running the analysis')
+        else:
+            st.write(f'{selected_stock} has changed :red[{change: .2f}%] since running the analysis')
